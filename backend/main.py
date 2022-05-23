7b663af0eed8876d592b9454b3b398f5e4487679
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-23 17:31:08 +0100

    creatin a database with one table recipe

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restx import Api, Resource
 from config import DevConfig
-
-
+from models import Recipe
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
+db.init_app(app)
+
 api=Api(app,doc='/docs')
 
 @api.route('/hello')
@@ -13,6 +16,12 @@ class HelloResource(Resource):
    def get(self):
       return {'message':'hello world'}
 
+@app.shell_context_processor
+def make_shell_context():
+   return {
+      'db': db,
+      'Recipe': Recipe
+   }
 
 if __name__=='__main__':
    app.run()
