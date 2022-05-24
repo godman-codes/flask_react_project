e18a44dab8e3cf2d94da8082a28605194a3c6af2
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-24 07:59:25 +0100

    intiated the flask migrate on the app and db migrated the new user table to the existing database

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,19 @@ from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Recipe
 from exts import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 db.init_app(app)
 
+#initiate migrate on the database and application
+migrate = Migrate(app, db)
+
 api=Api(app,doc='/docs')
 
-'''model serializer'''
+# model serializer
 recipe_model=api.model(
    'Recipe',
    {
