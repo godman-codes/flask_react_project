3787657d28eeb381efb902d2315ab6415a1c1d76
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-25 22:55:40 +0100

    imported bootsrap and router and specifiied some routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import CreateRecipe from "./components/CreateRecipe";
 
 const App = () => {
-   useEffect(() => {
-      fetch("/recipe/hello")
-         .then((res) => res.json())
-         .then((data) => {
-            console.log(data);
-            setMessage(data.message);
-         })
-         .catch((err) => console.log(err));
-   }, []);
-
-   const [message, setMessage] = useState("");
-   return <div>{message} </div>;
+   return (
+      <Router>
+         <div className="container">
+            <Navbar />
+            <Routes>
+               <Route path="/create_recipe" element={<CreateRecipe />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/signup" element={<SignUp />} />
+               <Route path="/" element={<Home />} />
+            </Routes>
+         </div>
+      </Router>
+   );
 };
 export default App;
