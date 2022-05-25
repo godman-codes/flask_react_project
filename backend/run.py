d9ccbeeaf61a6949f05132303d8981c06e69caa6
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-25 11:28:06 +0100

    fixed some bugs

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,6 @@
-from config import TestConfig
 from main import create_app
 from config import DevConfig
 
 if __name__=='__main__':
-   app=create_app(TestConfig)
+   app=create_app(DevConfig)
    app.run()
