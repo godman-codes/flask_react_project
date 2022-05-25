476c076050605b370d7c8fc18b0c8eaa06706f5e
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-25 22:56:15 +0100

    created components

---
+++ client/src/components/CreateRecipe.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateRecipe = () => {
+   return (
+      <div className="home">
+         <h1>CreateRecipe Page</h1>
+      </div>
+   );
+};
+
+export default CreateRecipe;
