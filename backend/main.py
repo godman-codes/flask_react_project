1272e6f87b27eab056477b1723a2df578af57ed6
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-25 11:27:39 +0100

    fixed some bugs

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-from flask_restx import Api, Resource, fields
+from flask import Flask
+from flask_restx import Api
 from models import Recipe, User
 from exts import db
 from flask_migrate import Migrate
