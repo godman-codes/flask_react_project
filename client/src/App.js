ebd08007309b645cf085977954013f122e2580cb
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-26 11:28:55 +0100

    addes some styles to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
@@ -10,15 +11,13 @@ import CreateRecipe from "./components/CreateRecipe";
 const App = () => {
    return (
       <Router>
-         <div className="container">
-            <Navbar />
-            <Routes>
-               <Route path="/create_recipe" element={<CreateRecipe />} />
-               <Route path="/login" element={<Login />} />
-               <Route path="/signup" element={<SignUp />} />
-               <Route path="/" element={<Home />} />
-            </Routes>
-         </div>
+         <Navbar />
+         <Routes>
+            <Route path="/create_recipe" element={<CreateRecipe />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
+         </Routes>
       </Router>
    );
 };
