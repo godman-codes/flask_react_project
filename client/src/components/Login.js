65cc2fc1a8f509a25f50530b7c0b417f94431d93
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-05-26 12:24:28 +0100

    created teh login form for the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+
+   const loginUser = () => {
+      console.log("form Submitted");
+      console.log(username, password);
+      setUsername("");
+      setPassword("");
+   };
+   const handleInput = (e) => {
+      const name = e.currentTarget.name;
+      const value = e.currentTarget.value;
+
+      if (name === "username") setUsername(value);
+      if (name === "password") setPassword(value);
+   };
    return (
-      <div className="home">
-         <h1>Login Page</h1>
+      <div className="container">
+         <div className="form">
+            <h1>Login Page</h1>
+            <form>
+               <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                     type="text"
+                     placeholder="Your Username"
+                     value={username}
+                     onChange={handleInput}
+                     name="username"
+                  />
+               </Form.Group>
+               <br />
+               <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                     type="password"
+                     placeholder="Your Password"
+                     value={password}
+                     onChange={handleInput}
+                     name="password"
+                  />
+               </Form.Group>
+               <br />
+               <Form.Group>
+                  <Button as="sub" variant="primary" onClick={loginUser}>
+                     Sign Up
+                  </Button>
+               </Form.Group>
+               <br />
+               <small>
+                  If you don't have an account <Link to="/signup">Sign Up</Link>
+               </small>
+            </form>
+         </div>
       </div>
    );
 };
